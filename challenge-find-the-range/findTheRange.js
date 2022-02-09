315dd19a269081cf97a2a8166b6ccf1b03978926
Bo Kim <bohyung0110@gmail.com>
2022-02-09 09:22:40 -0800

    FIRST COMMIT

---
+++ challenge-find-the-range/findTheRange.js
@@ -0,0 +1,15 @@
+// Starting array
+let array = [28, 43, -12, 30, 4, 0, -36]
+let small = 0;
+let large = 0;
+
+// Write your solution below:
+for (let i = 0; i <= array.length; i++){
+    if (small > array[i]){
+        small = array[i];
+    }
+    else if (large < array[i]){
+        large = array[i];
+    }
+}
+console.log(small, large);
